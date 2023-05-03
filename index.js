165f5fe17b1593c89ff6ecea953b3f0b82a7cc58
Jacob Ives <jacob.w.ives@gmail.com>
2023-05-03 16:02:49 -0600

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solution here
+function findMatching(drivers, string){
+  return drivers.filter((name) => name.toLowerCase() === string.toLowerCase())
+}
+
+function fuzzyMatch(drivers, string){
+    return drivers.filter((name=> name.slice(0, string.length) === string))
+}
+
+function matchName(drivers, string){
+    return drivers.filter(function(item){
+        return item.name === string;
+    })
+}
